ec19dde4d50aa935599439962a2ad913f9e8ae39
TransforMers.Phatntom <125521629+transformers001@users.noreply.github.com>
2023-02-27 14:45:37 -0500

    Update GitHubPracticeTwo.java
    This changes are from upstream git repo

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -5,5 +5,11 @@ public class GitHubPracticeTwo {
 	public int test(int a, int b) {
 		return a*b;
 	}
+	
+	public int test( int x, int y, String message){
+		System.out.println(message);
+		int z = x+y;
+		retunr z;
+	}
 
 }
